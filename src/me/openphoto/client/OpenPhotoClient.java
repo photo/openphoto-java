67ffa30a90940f10483774278cb6f008e6ae8759
Patrick Santana <patricksan@me.com>
2011-11-24 00:30:26 +0100

    Initial methods

--- src/me/openphoto/client/OpenPhotoClient.java
+++ src/me/openphoto/client/OpenPhotoClient.java
@@ -1,6 +1,34 @@
 package me.openphoto.client;
 
+import java.util.Map;
+
 public class OpenPhotoClient {
+	private String host;
+	private String consumerKey;
+	private String token;
+	private String tokenSecret;
+
 	public OpenPhotoClient(String host, String consumerKey, String token, String tokenSecret) {
+		this.host = host;
+		this.consumerKey = consumerKey;
+		this.token = token;
+		this.tokenSecret = tokenSecret;
+	}
+
+	public OpenPhotoClient() {
+		// first try to get the details from the system properties
+
+		// otherwise from ConfigurationProperties.java
+
+		// throw exception
+		throw new IllegalAccessError("No OAuth Credentials found");
+	}
+
+	public Map<String, String> rawGet(String getRequest) {
+		return null;
+	}
+
+	public Map<String, String> rawPost(String postRequest, String[] parameters) {
+		return null;
 	}
 }
---
+++ src/me/openphoto/client/util/ConfigurationProperties.java
@@ -0,0 +1,9 @@
+package me.openphoto.client.util;
+
+public class ConfigurationProperties {
+
+	public static final String CONSUMER_KEY = "";
+	public static final String CONSUMER_SECRET = "";
+	public static final String TOKEN = "";
+	public static final String TOKEN_SECRET = "";
+}
