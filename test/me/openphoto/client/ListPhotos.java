b15ee70248dcfcd34602fada70bf0ae15de952fa
Patrick Santana <patricksan@me.com>
2012-08-16 11:38:33 +0200

    #2: Added more testing and changed library to json-java

--- test/me/openphoto/client/ListPhotos.java
+++ test/me/openphoto/client/ListPhotos.java
@@ -2,6 +2,10 @@ package me.openphoto.client;
 
 import junit.framework.TestCase;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ListPhotos extends TestCase {
 
 	public void testGetAll() {
@@ -10,14 +14,24 @@ public class ListPhotos extends TestCase {
 		// map.put("returnSizes", "20x20");
 
 		String result = client.rawGet("/photos/list.json", null);
-		assertNotNull("Answer should not be null", result);
 		System.out.println(result);
+
+		// testing
+		assertNotNull("Answer should not be null", result);
+
 	}
-	
-	public void testGetAlbums() {
+
+	public void testGetAlbums() throws JSONException {
 		OpenPhotoClient client = new OpenPhotoClient();
 		String result = client.rawGet("/albums/list.json", null);
-		assertNotNull("Answer should not be null", result);
 		System.out.println(result);
+
+		JSONObject json = new JSONObject(result);
+		JSONArray data = json.getJSONArray("result");
+
+		// testing
+		assertNotNull("Answer should not be null", result);
+		assertTrue("Code is not 200", json.getInt("code") == 200);
+		assertTrue("Array should be empty or >1 ", data.length() >= 0);
 	}
 }
